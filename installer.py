1e3a767a8a6333b8be7e5b8a4dd17917912b939e
Sebastian <32680604+Sebireb@users.noreply.github.com>
2023-03-03 18:46:28 +0100

    Installer

--- installer.py
+++ installer.py
@@ -3,7 +3,7 @@ import PyInstaller.__main__
 if __name__ == '__main__':
     PyInstaller.__main__.run([
         'main.py',
-        '--onefile',
+        '--onedir',
         '--noconsole',
         '--name=PDFReorder'
     ])
